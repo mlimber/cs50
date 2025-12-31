e8f97367297e7bb67d6da38a8feb4682d53cdcb3
M Limber <mlimber@gmail.com>
2025-12-31 01:15:33 -0500

    Update conftest dirs

--- 0_search-degrees/test/conftest.py
+++ 0_search-degrees/test/conftest.py
@@ -2,6 +2,6 @@ import os
 import sys
 from pathlib import Path
 
-project_dir = Path(__file__).parent
+project_dir = Path(__file__).parent.parent
 sys.path.insert(0, str(project_dir))
 os.chdir(project_dir)
